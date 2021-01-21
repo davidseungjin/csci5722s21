d5dd37bb0ac298c25863735352a539b58eb322ff
David Lee <david.lee-5@colorado.edu>
2021-01-21 14:45:06 -0700

    initial copying the files

---
+++ HW1/vision_hwk1.m
@@ -0,0 +1,69 @@
+% This script creates a menu driven application
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% your information
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear all;close all;clc;
+
+% Display a menu and get a choice
+choice = menu('Choose an option', 'Exit Program', 'Load Image', ...
+    'Display Image', 'Mean Filter');  % as you develop functions, add buttons for them here
+ 
+% Choice 1 is to exit the program
+while choice ~= 1
+   switch choice
+       case 0
+           disp('Error - please choose one of the options.')
+           % Display a menu and get a choice
+           choice = menu('Choose an option', 'Exit Program', 'Load Image', ...
+    'Display Image', 'Mean Filter');  % as you develop functions, add buttons for them here
+        case 2
+           % Load an image
+           image_choice = menu('Choose an image', 'lena1', 'mandril1', 'sully', 'yoda', 'shrek');
+           switch image_choice
+               case 1
+                   filename = 'lena1.jpg';
+               case 4
+                   filename = 'yoda.bmp';
+               % fill in cases for all the images you plan to use
+           end
+           current_img = imread(filename);
+       case 3
+           % Display image
+           figure
+           imagesc(current_img);
+           if size(current_img,3) == 1
+               colormap gray
+           end
+           
+       case 4
+           % Mean Filter
+           
+           % 1. Ask the user for size of kernel
+           
+           % 2. Call the appropriate function
+           newImage = meanFilter(current_img, k_size); % create your own function for the mean filter
+           
+           % 3. Display the old and the new image using subplot
+           % ....
+           %subplot(...)
+           %imagesc(current_img)
+           
+           % subplot(...)
+           %imagesc(newImage)
+           
+           
+           % 4. Save the newImage to a file
+           
+              
+       case 5
+           %....
+           
+       %....
+   end
+   % Display menu again and get user's choice
+   choice = menu('Choose an option', 'Exit Program', 'Load Image', ...
+    'Display Image', 'Mean Filter');  % as you develop functions, add buttons for them here
+end
