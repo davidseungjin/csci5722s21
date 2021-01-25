38c2a6b9b554b1330fd591f0f4b9e274f3990ced
David Lee <david.lee-5@colorado.edu>
2021-01-25 12:45:49 -0700

    Task 13 updating

--- HW1/D_func/D_SwirlFilter.m
+++ HW1/D_func/D_SwirlFilter.m
@@ -1,20 +1,17 @@
 function [outImg] = D_SwirlFilter(inImg, factor, ox, oy)
-% Lecture note of Polytechnic University, Brooklyn
+% factor: [-1, 1]
+% Resource: 
+% 1. Lecture note of Polytechnic University, Brooklyn
+% 2. Lecture 1 on wk 3.
 % 
 % Direction:
-% 1. Using ox, oy and each x, y coordinates, predefined theta
-% 
+% 1. Using ox, oy, find min/max x, y index to compute
+%       radius = min(x, y, row-x, col-y)
+%       Only handle x, y having distance less than radius. Skip others.
 % 2. find the projected x, y as x1, y1
 % inverse rotation transformation is the starting point 
 % rotation = [cos(theta) sin(theta); -sin(theta) cos(theta)]
 % inv_rotation = [cos(theta2) -sin(theta2); sin(theta2) cos(theta2)]
-% where thata2 = theta * distance (distance from ox, oy)
-% Distance between (x1, y1) and the origin is same as the dist between
-% (x,y) and the origin, so the distance of (x1, y1) is compatible with
-% using theta2
-% distance = sqrt((x1 - ox)^2 + (y1 - oy)^2)
-% x = (x1 - ox)cos(theta2) - (y1 - oy)sin(theta2) + ox
-% y = (x1 - ox)sin(theta2) + (y1 - oy)cos(theta2) + oy
 % 
 % 3. After finding estimated (x, y), find the pixel value of (x, y)
 % should be by bilinear interpolation.
@@ -25,20 +22,28 @@ function [outImg] = D_SwirlFilter(inImg, factor, ox, oy)
 
 myinput = imread(inImg);
 [myrow, mycol, mychannel] = size(myinput);
+max_angle = 2 * pi * factor;
 
-outImg = zeros(myrow, mycol, mychannel, class(myinput));
-scale_factor = 1;
+% outImg = zeros(myrow, mycol, mychannel, class(myinput));
+outImg = myinput;
+radius = min([ox-1, oy-1, myrow-ox+1, mycol-oy+1]);
+
 
 for i = 1:myrow
     for j = 1:mycol
-        distance = sqrt((i-ox)^2 + (j-oy)^2);
-%         myangle = distance * factor;
-        myangle = mod(factor * distance, 2 * pi);
-        x = (i - ox)*cos(myangle) - (j - oy)*sin(myangle) + ox;
-        y = (i - ox)*sin(myangle) + (j - oy)*cos(myangle) + oy;
-        if x >= 1 && x <= myrow && y >= 1 && y <= mycol
-            [TL_x, TL_y, TR_x, TR_y, BL_x, BL_y, BR_x, BR_y, col_ratio, row_ratio] = D_sampleBilinear(x, y, myrow, mycol, scale_factor, scale_factor);
-            outImg(i,j,:) = round(row_ratio * (col_ratio * myinput(TL_x, TL_y, :) + (1-col_ratio) * myinput(TR_x, TR_y, :)) + (1-row_ratio) * (col_ratio * myinput(BL_x, BL_y, :) + (1-col_ratio) * myinput(BR_x, BR_y, :)));
+        if (ox-radius <= i <= ox+radius) & (oy-radius <= j <= oy+radius)
+            distance = sqrt((i-ox)^2 + (j-oy)^2);
+            if distance <= radius
+                myangle = max_angle * distance / radius;
+                x = (i - ox)*cos(myangle) - (j - oy)*sin(myangle) + ox;
+                y = (i - ox)*sin(myangle) + (j - oy)*cos(myangle) + oy;
+                [TL_x, TL_y, TR_x, TR_y, BL_x, BL_y, BR_x, BR_y, col_ratio, row_ratio] ...
+                    = D_sampleBilinear(x, y, myrow, mycol, 1, 1);
+                outImg(i,j,:) = round(row_ratio * (col_ratio * myinput(TL_x, TL_y, :) + ...
+                    (1-col_ratio) * myinput(TR_x, TR_y, :)) + ...
+                    (1-row_ratio) * (col_ratio * myinput(BL_x, BL_y, :) + ...
+                    (1-col_ratio) * myinput(BR_x, BR_y, :)));
+            end
         end
     end
 end
