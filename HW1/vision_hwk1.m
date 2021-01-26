aaa1ceef66d1ebfea5bd4058699cc4712e162c93
alec-bell <alec.timothy.bell@gmail.com>
2021-01-26 00:01:51 -0700

    Clean up

--- HW1/vision_hwk1.m
+++ HW1/vision_hwk1.m
@@ -1,69 +1,274 @@
 % This script creates a menu driven application
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-% your information
-%
+% Names: Alec Bell, David Lee
+% Course #: CSCI 5722
+% Assignment #: 1
+% Instructor: Fleming
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-clear all;close all;clc;
+clear all;
+close all;
+clc;
+
+% Declare lambda for the menu so we don't have to define these menu options
+% multiple times.
+main_menu = @() menu(...
+    'Choose an option', ...
+    'Exit Program', ...
+    'Load Image', ...
+    'Display Image', ...
+    'Brighten_L', ...
+    'Brighten_NL', ...
+    'Invert_L', ...
+    'Invert_NL', ...
+    'AddRandomNoise_L', ...
+    'Luminance_L', ...
+    'RedFilter', ...
+    'BinaryMask', ...
+    'MeanFilter', ...
+    'Frosty', ...
+    'ScaleNearest', ...
+    'ScaleBilinear', ...
+    'SwirlFilter', ...
+    'FamousMe' ...
+);
 
 % Display a menu and get a choice
-choice = menu('Choose an option', 'Exit Program', 'Load Image', ...
-    'Display Image', 'Mean Filter');  % as you develop functions, add buttons for them here
+choice = main_menu();
  
 % Choice 1 is to exit the program
 while choice ~= 1
-   switch choice
-       case 0
-           disp('Error - please choose one of the options.')
-           % Display a menu and get a choice
-           choice = menu('Choose an option', 'Exit Program', 'Load Image', ...
-    'Display Image', 'Mean Filter');  % as you develop functions, add buttons for them here
+    switch choice
+        case 0
+            disp('Error - please choose one of the options.')
+            choice = main_menu();
         case 2
-           % Load an image
-           image_choice = menu('Choose an image', 'lena1', 'mandril1', 'sully', 'yoda', 'shrek');
-           switch image_choice
+            % Load an image
+            image_choice = menu('Choose an image', 'lena1', 'mandrill1', 'sully', 'yoda', 'shrek', 'wrench', 'mars');
+            switch image_choice
                case 1
-                   filename = 'lena1.jpg';
+                   filename = 'images/lena1.jpg';
+               case 2
+                   filename = 'images/mandrill1.jpg';
+               case 3
+                   filename = 'images/sully.bmp';
                case 4
-                   filename = 'yoda.bmp';
-               % fill in cases for all the images you plan to use
-           end
-           current_img = imread(filename);
-       case 3
-           % Display image
-           figure
-           imagesc(current_img);
-           if size(current_img,3) == 1
+                   filename = 'images/yoda.bmp';
+               case 5
+                   filename = 'images/shrek.bmp';
+               case 6
+                   filename = 'images/wrench1.jpg';
+               case 7
+                   filename = 'images/mars.jpg';
+            end
+            current_img = imread(filename);
+        case 3
+            % Display image
+            figure
+            imshow(current_img);
+            if size(current_img,3) == 1
                colormap gray
-           end
-           
+            end
        case 4
-           % Mean Filter
+            % Brighten (with loops)
+            % Get user input
+            answer = inputdlg('Enter an amount between (-255,+255) to adjust the brightness:', ...
+               'Brightness Adjustment', 1, {'0'});
            
-           % 1. Ask the user for size of kernel
+            % Convert input to number, generate new image
+            brightness = str2num(answer{1});
+            newImage = makeBright_L(current_img, brightness);
+            
+            % Compare two images
+            displaySideBySide(current_img, newImage);
+            
+            % Save the new image
+            saveImage(newImage)
+        case 5
+            % Brighten (without loops)
+            % Get user input
+            answer = inputdlg('Enter an amount between (-255,+255) to adjust the brightness:', ...
+               'Brightness Adjustment', 1, {'0'});
            
-           % 2. Call the appropriate function
-           newImage = meanFilter(current_img, k_size); % create your own function for the mean filter
+            % Convert input to number, generate new image
+            brightness = str2num(answer{1});
+            newImage = makeBright_NL(current_img, brightness);
+            
+            % Compare two images
+            displaySideBySide(current_img, newImage);
+            
+            % Save the new image
+            saveImage(newImage)
+        case 6
+            % Invert (with loops)
+            newImage = invert_L(current_img);
+            
+            % Compare two images
+            displaySideBySide(current_img, newImage);
+            
+            % Save the new image
+            saveImage(newImage)
+        case 7
+            % Invert (without loops)
+            newImage = invert_NL(current_img);
+            
+            % Compare two images
+            displaySideBySide(current_img, newImage);
+            
+            % Save the new image
+            saveImage(newImage)
+        case 8
+            % Add Random Noise (without loops)
+            newImage = addRandomNoise_L(current_img);
+            
+            % Compare two images
+            displaySideBySide(current_img, newImage);
+            
+            % Save the new image
+            saveImage(newImage)
+        case 9
+            % Luminance (without loops)
+            newImage = luminance_L(current_img);
+            
+            % Compare two images
+            % Have to use different image plot function to avoid "imagesc"
+            % applying a colormap.
+            displaySideBySide(current_img, newImage, @(img) imshow(img));
+            
+            % Save the new image
+            saveImage(newImage)
+        case 10
+            % Red Filter (without loops)
+            % Get user input
+            answer = inputdlg('Enter an amount between (0,1) to adjust the red-ness:', ...
+               'Red Filter', 1, {'0.5'});
            
-           % 3. Display the old and the new image using subplot
-           % ....
-           %subplot(...)
-           %imagesc(current_img)
+            % Convert input to number, generate new image
+            redVal = str2double(answer{1});
+            newImage = redFilter(current_img, redVal);
+            
+            % Compare two images
+            displaySideBySide(current_img, newImage);
+            
+            % Save the new image
+            saveImage(newImage)
+        case 11
+            % Binary Mask
+            newImage = binaryMask(current_img);
+            
+            % Compare two images
+            displaySideBySide(current_img, newImage, @(img) imshow(img));
+            
+            % Save the new image
+            saveImage(newImage)
+        case 12
+            % Mean Filter
+            % Get user input
+            answer = inputdlg('Enter an kernel_size (int > 2):', ...
+               'Mean Filter', 1, {'3'});
            
-           % subplot(...)
-           %imagesc(newImage)
+            % Convert input to number, generate new image
+            kernel_size = str2num(answer{1});
+            newImage = meanFilter(current_img, kernel_size);
+            
+            % Compare two images
+            displaySideBySide(current_img, newImage);
+            
+            % Save the new image
+            saveImage(newImage)
+        case 13
+            % Frosty Filter
+            % Get user input
+            answer = inputdlg({'n:','m;'}, ...
+               'Frosty Filter Dimensions', 2, {'4','4'});
            
+            % Convert input to number, generate new image
+            n = str2num(answer{1});
+            m = str2num(answer{2});
+            newImage = frosty(current_img, n, m);
+            
+            % Compare two images
+            displaySideBySide(current_img, newImage);
+            
+            % Save the new image
+            saveImage(newImage)
+        case 14
+            % Scale Nearest
+            % Get user input
+            answer = inputdlg('Enter scaling factor:', ...
+               'Scale Nearest', 1, {'1'});
            
-           % 4. Save the newImage to a file
+            % Convert input to number, generate new image
+            factor = str2num(answer{1});
+            newImage = scaleNearest(current_img, factor);
+            
+            % Compare two images
+            displaySideBySide(current_img, newImage);
+            
+            % Save the new image
+            saveImage(newImage)
+        case 15
+            % Scale Bilinear
+            % Get user input
+            answer = inputdlg('Enter scaling factor:', ...
+               'Scale Bilinear', 1, {'1'});
            
-              
-       case 5
-           %....
+            % Convert input to number, generate new image
+            factor = str2num(answer{1});
+            newImage = scaleBilinear(current_img, factor);
+            
+            % Compare two images
+            displaySideBySide(current_img, newImage);
+            
+            % Save the new image
+            saveImage(newImage)
+        case 16
+            % Swirl Filter
+            % Find center of image to auto-supply them in inputs for ease
+            [numRows, numColumns, numColorChannels] = size(current_img);
+            
+            % Get user input
+            answer = inputdlg({'factor:', 'ox', 'oy'}, ...
+               'Swirl Filter', 3, {'1', int2str(numRows/2), int2str(numColumns/2)});
            
-       %....
-   end
-   % Display menu again and get user's choice
-   choice = menu('Choose an option', 'Exit Program', 'Load Image', ...
-    'Display Image', 'Mean Filter');  % as you develop functions, add buttons for them here
+            % Convert input to number, generate new image
+            factor = str2num(answer{1});
+            ox = str2num(answer{2});
+            oy = str2num(answer{3});
+            newImage = swirlFilter(current_img, factor, ox, oy);
+            
+            % Compare two images
+            displaySideBySide(current_img, newImage);
+            
+            % Save the new image
+            saveImage(newImage)
+        case 17
+            % Famous Me
+            me = imread('images/alec.png');
+            
+            % Get user input
+            answer = inputdlg({'factor:', 'x', 'y'}, ...
+               'Famous Me', 3, {'4', '1500', '1000'});
+           
+            % Convert input to number, generate new image
+            factor = str2num(answer{1});
+            x = str2num(answer{2});
+            y = str2num(answer{3});
+            newImage = famousMe(current_img, me, x, y, factor);
+            
+            % Compare two images
+            figure
+            subplot(1,3,1)
+            imagesc(me);
+            subplot(1,3,2)
+            imagesc(current_img);
+            subplot(1,3,3)
+            imagesc(newImage);
+            
+            % Save the new image
+            saveImage(newImage)
+            
+    end
+    
+    choice = main_menu();
 end
