1f1c13d9a13e8d6d6196c6d49e72776d008e5db6
David (Seungjin) Lee <41690461+davidseungjin@users.noreply.github.com>
2021-01-26 13:57:58 -0700

    Update sampleBilinear_D.m

--- HW1/functions/sampleBilinear_D.m
+++ HW1/functions/sampleBilinear_D.m
@@ -17,14 +17,6 @@ TopRight_y = floor(exact_y);TopRight_x =ceil(exact_x);
 BottomLeft_y = ceil(exact_y);BottomLeft_x = floor(exact_x);
 BottomRight_y = ceil(exact_y);BottomRight_x = ceil(exact_x);
 
-if BottomRight_y > numRows || BottomRight_x > numColumns
-    BottomRight_y
-    BottomRight_x
-    disp('this case')
-end
-
-
-
 yAxis_ratio = exact_y - TopLeft_y;
 xAxis_ratio = exact_x - TopLeft_x;
 
@@ -38,17 +30,6 @@ pixel = round((1-yAxis_ratio) * ...
     xAxis_ratio * ...
     (img(BottomRight_y, BottomRight_x, :) - img(BottomLeft_y, BottomLeft_x, :))));
 
-
-% % Grab all four pixels that these indices could possibly round to.
-% pixels = [ ...
-%     img(floor(original_i), floor(original_j), :), ...
-%     img(floor(original_i), ceil(original_j), :), ...
-%     img(ceil(original_i), floor(original_j), :), ...
-%     img(ceil(original_i), ceil(original_j), :) ...
-% ];
-% 
-% % Calculate the mean value of all four pixels.
-% pixel = [mean(pixels(:,:,1)), mean(pixels(:,:,2)), mean(pixels(:,:,3))];
 
 end
 
